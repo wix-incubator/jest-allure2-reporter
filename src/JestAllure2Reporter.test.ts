49060ede4c1f56954c0ae91bbf4f44456808691f
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-12-17 18:52:56 +0200

    wip: more tests

--- src/JestAllure2Reporter.test.ts
+++ src/JestAllure2Reporter.test.ts
@@ -4,25 +4,104 @@ import path from 'path';
 
 import fs from 'fs-extra';
 import _ from 'lodash';
+import tempfile from 'tempfile';
 
 import JestAllure2Reporter from './JestAllure2Reporter';
+import type { JestAllure2ReporterOptions } from './JestAllure2ReporterOptions';
 
 const rootDirectory = path.join(__dirname, '..');
 const testReporterCallsPath = path.join(rootDirectory, '__fixtures__/test-reporter-calls.json');
-const allureDirectory = path.join(rootDirectory, '__fixtures__/allure-results');
 
 describe('JestAllure2Reporter', () => {
-  let reporter!: JestAllure2Reporter;
-  let allureResults!: Record<string, unknown>[];
+  let allureResultsDirectory: string;
 
-  beforeAll(async () => {
-    await fs.remove(allureDirectory);
-    reporter = new JestAllure2Reporter({ rootDir: rootDirectory } as any, {
-      resultsDir: allureDirectory,
+  describe('with default options', () => {
+    beforeAll(() => simulateTestRun({}));
+
+    afterAll(async () => {
+      await fs.remove(allureResultsDirectory);
+    });
+
+    test('statuses', async () => {
+      const results = await bySuite('statuses');
+
+      expect(results).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            name: 'root broken test',
+            status: 'broken',
+            statusDetails: expect.objectContaining({
+              message: 'Error: Simulated error',
+            }),
+          }),
+          expect.objectContaining({
+            name: 'root failed test',
+            status: 'failed',
+            statusDetails: expect.objectContaining({
+              message: expect.any(String),
+            }),
+          }),
+          expect.objectContaining({
+            name: 'root skipped test',
+            status: 'skipped',
+          }),
+          expect.objectContaining({
+            name: 'root passed test (600ms)',
+            status: 'passed',
+          }),
+        ]),
+      );
     });
   });
 
-  beforeAll(async () => {
+  describe('with options.errorsAsFailedAssertions = true', () => {
+    beforeAll(() =>
+      simulateTestRun({
+        errorsAsFailedAssertions: true,
+      }),
+    );
+
+    afterAll(async () => {
+      await fs.remove(allureResultsDirectory);
+    });
+
+    test('statuses', async () => {
+      const results = await bySuite('statuses');
+      expect(results).not.toEqual(
+        expect.arrayContaining([expect.objectContaining({ status: 'broken' })]),
+      );
+      expect(results).toEqual(
+        expect.arrayContaining([expect.objectContaining({ status: 'failed' })]),
+      );
+    });
+  });
+
+  async function bySuite(name: string) {
+    const results = await readAllureResults();
+    return results.filter((result) => {
+      const labels = _.get(result, 'labels', []) as object[];
+      const suiteLabel = _.find(labels, { name: 'suite' }) as any;
+      return suiteLabel?.value === `__fixtures__/tests/${name}.test.js`;
+    });
+  }
+
+  async function readAllureResults(): Promise<Record<string, unknown>[]> {
+    const files = await fs.readdir(allureResultsDirectory);
+    return await Promise.all(
+      files
+        .filter((f) => f.endsWith('.json'))
+        .map(async (file) => {
+          const content = await fs.readFile(path.join(allureResultsDirectory, file));
+          return JSON.parse(content as any);
+        }),
+    );
+  }
+
+  async function simulateTestRun(options: Partial<JestAllure2ReporterOptions>) {
+    options.resultsDir = allureResultsDirectory = tempfile('');
+    const globalConfig = { rootDir: rootDirectory } as any;
+    const reporter = new JestAllure2Reporter(globalConfig, options);
+
     const testReporterCalls = fs
       .readFileSync(testReporterCallsPath, 'utf8')
       .split('\n')
@@ -35,57 +114,5 @@ describe('JestAllure2Reporter', () => {
         await (reporter as any)[call.method](...call.params);
       }
     }
-  });
-
-  beforeAll(async () => {
-    const files = await fs.readdir(allureDirectory);
-    allureResults = await Promise.all(
-      files
-        .filter((f) => f.endsWith('.json'))
-        .map(async (file) => {
-          const content = await fs.readFile(path.join(allureDirectory, file));
-          return JSON.parse(content as any);
-        }),
-    );
-  });
-
-  test.each([
-    [
-      'statuses',
-      expect.arrayContaining([
-        expect.objectContaining({
-          name: 'root broken test',
-          status: 'broken',
-          statusDetails: expect.objectContaining({
-            message: 'Error: Simulated error',
-          }),
-        }),
-        expect.objectContaining({
-          name: 'root failed test',
-          status: 'failed',
-          statusDetails: expect.objectContaining({
-            message: expect.any(String),
-          }),
-        }),
-        expect.objectContaining({
-          name: 'root skipped test',
-          status: 'skipped',
-        }),
-        expect.objectContaining({
-          name: 'root passed test (600ms)',
-          status: 'passed',
-        }),
-      ]),
-    ],
-  ])('should generate correct results for: %s.test.js', (testName, expected) => {
-    expect(filterBySuite(testName)).toEqual(expected);
-  });
-
-  function filterBySuite(name: string) {
-    return allureResults.filter((result) => {
-      const labels = _.get(result, 'labels', []) as object[];
-      const suiteLabel = _.find(labels, { name: 'suite' }) as any;
-      return suiteLabel?.value === `__fixtures__/tests/${name}.test.js`;
-    });
   }
 });
