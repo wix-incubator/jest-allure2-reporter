4775877dc30aa40dcc60c017df8db53e0faf5779
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-12-18 18:51:41 +0200

    wip

--- fixture-results.jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@jest/types').Config} */
-module.exports = {
-  ...require('./jest.config'),
-
-  testMatch: ['<rootDir>/__fixtures__/tests/**/*.test.{js,ts}'],
-  reporters: ['<rootDir>/scripts/dump-reporter.js'],
-};
