aa02155f0bdddce2caf341ee696d5bd1184b89a4
Yaroslav Serhieiev <noomorph@gmail.com>
2024-03-15 22:11:24 +0200

    refactor: move autoIndent

--- src/reporter-plugins/utils/extractTypeScriptCode.ts
+++ src/reporter-plugins/utils/extractTypeScriptCode.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line node/no-unpublished-import
 import type TypeScript from 'typescript';
 
+import { autoIndent } from './autoIndent';
+
 export async function extractTypeScriptCode(
   ts: typeof TypeScript,
   ast: TypeScript.SourceFile | undefined,
@@ -34,21 +36,4 @@ export async function extractTypeScriptCode(
       ast.text.slice(expression.getStart(), expression.getEnd()),
     );
   }
-}
-
-function autoIndent(text: string) {
-  const [first, ...rest] = text.split('\n');
-  const indent = detectIndent(rest);
-  if (indent > 0) {
-    return [first, ...rest.map((line) => line.slice(indent))].join('\n');
-  }
-
-  return text;
-}
-
-function detectIndent(lines: string[]) {
-  return lines.reduce((indent, line) => {
-    const size = line.length - line.trimStart().length;
-    return size < indent ? size : indent;
-  }, Number.POSITIVE_INFINITY);
 }
